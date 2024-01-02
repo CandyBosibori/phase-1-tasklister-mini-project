dd0dfbe9410ef12527b3363e99f558c3ee9e7e6b
Candy <candy.ongaro@student.moringaschool.com>
2024-01-02 23:02:51 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+document.querySelector("form").addEventListener("submit", function(e){
+  e.preventDefault()
+handleToDo(e.target.new-task-description.value)
+})
+
 });
+
+function handleToDo(ToDo){
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.textContent = "x"
+  p.textContent = ToDo
+  document.querySelector("#tasks").appendChild(p)
+}
